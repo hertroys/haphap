181806645de091244c0d1abe40a0d86385057580
hertroys <hertroys@gmail.com>
2016-10-14 20:07:36 +0200

    Made Tag validable.

--- app/models/Tag.php
+++ app/models/Tag.php
@@ -5,5 +5,15 @@ use Eloquent;
 
 class Tag extends Eloquent
 {
+    use Validable;
 
+    /**
+     * Return the validation rules for the model.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return ['name' => 'required | unique:tags,name,'.$this->id];
+    }
 }
